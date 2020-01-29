82b2a21b8e7d297cdd41892a341dbd246825a689
tlylt <liu_yongliang@hotmail.com>
2020-01-29 22:27:49 +0800

    completed subject

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,213 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <current-song :song="currentSong" v-if="currentSong" />
+    <song-list
+      :songs="songs"
+      :currentSong="currentSong"
+      @handlePlay="handlePlay"
+      @handleDelete="handleDelete"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import CurrentSong from "@/components/CurrentSong";
+import SongList from "@/components/SongList";
+// import _ from "lodash";
+// import axios from "axios";
+import { mapState } from "vuex";
 export default {
   name: "app",
+  data() {
+    return {
+      // currentSong: null,
+      audioElement: null
+      // songs: null
+    };
+  },
+  computed: {
+    ...mapState(["songs", "currentSong"])
+  },
+  methods: {
+    handlePlay: function(payload) {
+      if (this.audioElement == null) {
+        this.audioElement = new Audio(payload.music_url);
+        this.audioElement.play();
+      } else {
+        if (payload == this.currentSong) {
+          if (this.audioElement.paused) {
+            this.audioElement.play();
+          } else {
+            this.audioElement.pause();
+          }
+        } else {
+          this.audioElement.src = payload.music_url;
+          this.audioElement.play();
+        }
+      }
+      this.$store.dispatch("changeSong", payload);
+      // this.currentSong = payload;
+      this.audioElement.addEventListener("ended", () => {
+        this.$store.dispatch("changeSong", null);
+        // this.currentSong = null;
+        this.audioElement = null;
+      });
+    },
+    handleDelete: function(payload) {
+      this.$store.dispatch("deleteSong", payload);
+      // const updatedArray = _.without(this.songs, payload);
+      // this.songs = updatedArray;
+    }
+  },
+  created() {
+    this.$store.dispatch("fetchSongs");
+
+    // axios({
+    //   method: "get",
+    //   url: "https://orangevalleycaa.org/api/music",
+    //   params: { order: "name" }
+    // }).then(response => (this.songs = response.data));
+
+    // axios
+    //   .get("https://orangevalleycaa.org/api/music")
+    //   .then(response => (this.songs = response.data));
+
+    // using the axios method from local files
+    // axios({ method: "get", url: "./data.json" }).then(
+    //   response => (this.songs = response.data)
+    // );
+
+    // using the fetch api method
+    // fetch("./data.json")
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     this.songs = response;
+    //   })
+    //   // eslint-disable-next-line no-console
+    //   .catch(error => console.log(error));
+  },
   components: {
-    HelloWorld
+    // FontAwesomeIcon,
+    CurrentSong,
+    SongList
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans:300&display=swap");
+// colors
+$primary: #00adb5;
+$secondary: #f8b500;
+$body-bg: #2a2d33;
+$text-body: #fff;
+$body-color: $text-body;
+$border-radius: 8px;
+
+//overrides
+
+$font-family-headlines: "Fjalla One", -apple-system, BlinkMacSystemFont,
+  "Segoe UI", "Roboto", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+  "Segoe UI Emoji", "Segoe UI Symbol";
+$font-family-base: "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
+  "Roboto", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+  "Segoe UI Emoji", "Segoe UI Symbol";
+
+$headings-font-family: $font-family-headlines;
+$headings-font-weight: 400;
+$line-height-base: 1.4;
+
+$list-group-active-bg: #222;
+$list-group-bg: transparent;
+$list-group-border-radius: 0;
+$list-group-hover-bg: #444;
+$list-group-action-color: #bbb;
+
+@import "node_modules/bootstrap/scss/bootstrap";
+
+//currentsong
+.currentsong-background {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.currentsong-backgroundimage {
+  height: 100%;
+  width: 100%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  filter: blur(8px) grayscale(50%);
+  transform: scale(1.15);
+}
+
+.currentsong .song-info {
+  background-color: #000;
+}
+
+.currentsong .song-name {
+  font-size: 2.5em;
+}
+.currentsong .song-description {
+  font-size: 0.9em;
+  line-height: 120%;
+}
+
+// SongList
+.songlist .song:active {
+  background: $gray-700;
+}
+
+.songlist .song-info-name {
+  font-size: 1.3em;
+  color: $gray-400;
+}
+
+.songlist .song-info-creator {
+  font-size: 0.9em;
+}
+
+.songlist .thumbnail {
+  width: 60px;
+  height: 60px;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.songlist .song .duration {
+  color: $secondary;
+}
+
+.songlist .song .trash {
+  display: none;
+}
+
+.songlist .song:not(.active):hover .trash {
+  display: block;
+  cursor: pointer;
+}
+
+.songlist .song .play,
+.songlist .song:hover.active .play {
+  display: none;
+}
+.songlist .song:hover .play {
+  display: block;
+  cursor: pointer;
+}
+.songlist .song .pause {
+  display: none;
+}
+
+.songlist .song:hover.active .pause {
+  display: block;
+  cursor: pointer;
+}
+
+.songlist .song.active .play,
+.songlist .song.active {
+  display: block;
 }
 </style>
---
+++ src/store.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import _ from "lodash";
+
+Vue.use(Vuex);
+
+export default new Vuex.store({
+  state: {
+    songs: [],
+    currentSong: null
+  },
+  mutations: {
+    SET_SONGS(state, payload) {
+      state.songs = payload;
+    },
+    CHANGE_CURRENT_SONG(state, payload) {
+      state.currentSong = payload;
+    }
+  },
+  actions: {
+    changeSong({ commit }, payload) {
+      commit("CHANGE_CURRENT_SONG", payload);
+    },
+    fetchSongs({ commit }) {
+      axios({
+        method: "get",
+        url: "https://orangevalleycaa.org/api/music",
+        params: { order: "name" }
+      }).then(response => commit("SET_SONGS", response.data));
+    },
+    deleteSong({ commit }, payload) {
+      let songs = _.without(this.state.songs, payload);
+      commit("SET_SONGS", songs);
+    }
+  }
+});
